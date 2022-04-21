725a1f5662942f936abdd0597981ec64066cc312
Gabriel Correa <vdvsa159946@gmail.com>
2022-04-21 16:08:16 -0300

    finalizando as animações e scroll

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,8 @@
 
-
+/*Toggle menu ================================== */
 const botao = document.querySelector('#btn-mobile');
 const menuLinks = document.querySelectorAll('#list-menu li a');
 const nav = document.querySelector('#nav');
-console.log(menuLinks)
 
 
 function toggleMenu(event){
@@ -37,6 +36,7 @@ for(const links of menuLinks){
 botao.addEventListener('touchstart', toggleMenu);
 botao.addEventListener('click', toggleMenu);
 
+
 /*const links = document.querySelectorAll('nav ul li a');
 
 for (const link of links){
@@ -46,3 +46,52 @@ for (const link of links){
 }
 
 */
+
+/* Animar ao Scroll ========================================== */
+
+/*1 - Selecionar elementos que devem ser animados
+2 - Definir a classe que é adicionada durante a animação
+3 - Criar função de animação
+3.1 - Verificar a distância entre a barra de scroll e o topo do site
+3.2 - Verificar se a distância do 3.1 + Offset é maior do que a distância entre o elemento e o Topo da Página.
+3.3 - Se verdadeiro adicionar classe de animação, remover se for falso.
+4 - Ativar a função de animação toda vez que o usuário utilizar o Scroll
+5 - Otimizar ativação
+*/
+
+const debounce = function(func, wait, immediate) {
+  let timeout;
+  return function(...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+const target = document.querySelectorAll("[data-anime]");
+const animationClass = 'animate';
+
+function animeScroll(){
+  const windowTop = window.pageYOffset + ((window.innerHeight * 4) / 4);
+  target.forEach(function(element){
+    if((windowTop) > element.offsetTop){
+      element.classList.add(animationClass);
+    }else{
+      element.classList.remove(animationClass);
+    }
+  })
+}
+
+animeScroll();
+
+if(target.length){
+  window.addEventListener('scroll', debounce(function(){
+  animeScroll();
+  }, 80));
+}
