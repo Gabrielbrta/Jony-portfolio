b924e8b4c52f1435886e35aeb033ebf6eb4dc4f8
Gabriel Correa <vdvsa159946@gmail.com>
2022-05-04 21:31:06 -0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -39,7 +39,7 @@ function toggleMenu(event){
     
 }
 
-botao.addEventListener('touchstart', toggleMenu,{passive:true});
+botao.addEventListener('touchstart', toggleMenu,{passive:false});
 botao.addEventListener('click', toggleMenu, {passive:true});
 
 /*Fechar menu  --------------------------------------------------------------*/
@@ -53,29 +53,6 @@ botao.addEventListener('click', toggleMenu, {passive:true});
 /*--------------------------------------------------------------*/
 
 
-
-
-/*const links = document.querySelectorAll('nav ul li a');
-
-for (const link of links){
-    link.addEventListener('click', function(){
-    nav.classList.remove('show')    
-    })
-}
-
-*/
-
-/* Animar ao Scroll ========================================== */
-
-/*1 - Selecionar elementos que devem ser animados
-2 - Definir a classe que é adicionada durante a animação
-3 - Criar função de animação
-3.1 - Verificar a distância entre a barra de scroll e o topo do site
-3.2 - Verificar se a distância do 3.1 + Offset é maior do que a distância entre o elemento e o Topo da Página.
-3.3 - Se verdadeiro adicionar classe de animação, remover se for falso.
-4 - Ativar a função de animação toda vez que o usuário utilizar o Scroll
-5 - Otimizar ativação
-*/
 
 const debounce = function(func, wait, immediate) {
   let timeout;
@@ -111,6 +88,5 @@ animeScroll();
 if(target.length){
   window.addEventListener('scroll',debounce(function(){
     animeScroll();
-    console.log(animeScroll)
   }, 50),{passive: true})
 }
