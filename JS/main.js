a3678a3a37d3ddaaab8486fa6206f9b45aae1999
Gabriel Correa <vdvsa159946@gmail.com>
2022-06-04 00:09:46 -0300

    refatorando o javascript e css
    correção de alguns elementos css e refatorando algumas funcionalidades

--- JS/main.js
+++ JS/main.js
@@ -1,57 +1,57 @@
+document.documentElement.className = ' js';
 
-/*Toggle menu ================================== */
+
 const botao = document.querySelector('#btn-mobile');
 const menuLinks = document.querySelectorAll('#list-menu li a[href^="#"]');
 const nav = document.querySelector('#nav');
 
-/* Scroll Suave -------------------------------------------------------*/
-menuLinks.forEach(item => {
-  item.addEventListener('click', scrollToId, {passive:false})
-})
 
-function scrollToId(event){
-  event.preventDefault();
-  const element = event.target;
-  const id = element.getAttribute('href');
-  const to = document.querySelector(id).offsetTop;
-
-  window.scroll(0, to - 75);
-}
-/* ----------------------------------------------------------------- */
-function toggleMenu(event){
-    if(event.type === 'touchstart')event.preventDefault();
-
+function initScrollSuave(){
+  if(menuLinks.length){
+      menuLinks.forEach(item => {
+      item.addEventListener('click', scrollToId, {passive:false})
+    })
     
-    const nav = document.querySelector('#nav');
+    function scrollToId(event){
+      event.preventDefault();
+      const element = event.target;
+      const id = element.getAttribute('href');
+      const to = document.querySelector(id).offsetTop;
+    
+      window.scroll(0, to - 75);
+    }
+  }
+}
+initScrollSuave()
 
-   let menuLinks = document.querySelectorAll('#list-menu li a'); 
+
+function initToggleMenu(){
+  const clientEvent = ['click', 'touchStart']
+  clientEvent.forEach(event => {
+    botao.addEventListener(event, toggleMenu);
+  menuLinks.forEach(links => {
+    links.addEventListener(event, toggleMenu);
+    })
+  })
+
+  function toggleMenu(event){
+    const nav = document.querySelector('#nav');
+    if(event.type === clientEvent[0] && event.type === clientEvent[2]);
+    event.preventDefault();
 
     nav.classList.toggle('active');
     const active = nav.classList.contains('active')
     event.currentTarget.setAttribute('aria-expanded', active)
-
+  
     if(active){
       event.currentTarget.setAttribute('aria-label', 'fechar menu')
-
+      
     }else{
-        event.currentTarget.setAttribute('aria-label', 'Abrir menu')
+      event.currentTarget.setAttribute('aria-label', 'Abrir menu')
     }
-    
-}
-
-botao.addEventListener('touchstart', toggleMenu,{passive:false});
-botao.addEventListener('click', toggleMenu, {passive:true});
-
-/*Fechar menu  --------------------------------------------------------------*/
-
-  for(const links of menuLinks){
-    links.addEventListener('click', function(){
-      nav.classList.remove('active');
-    },{passive:true})
   }
-
-/*--------------------------------------------------------------*/
-
+}
+initToggleMenu();
 
 
 const debounce = function(func, wait, immediate) {
@@ -69,24 +69,25 @@ const debounce = function(func, wait, immediate) {
   };
 };
 
-const target = document.querySelectorAll("[data-anime]");
-const animationClass = 'animate';
+function initAnimeScroll(){
+  const target = document.querySelectorAll("[data-anime]");
+  if(target.length){
+    const animationClass = 'animate';
 
-function animeScroll(){
-  const windowTop = window.pageYOffset + ((window.innerHeight * 3.5) / 4);
-  target.forEach(function(element){
-    if((windowTop) > element.offsetTop){
-      element.classList.add(animationClass);
-    }else{
-      element.classList.remove(animationClass);
-    }
-  })
+  function animeScroll(){
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3.5) / 4);
+    target.forEach(function(element){
+      if ((windowTop) > element.offsetTop){
+        element.classList.add(animationClass);
+      }
+    })
+  }
+  animeScroll();
 }
-
-animeScroll();
-
-if(target.length){
-  window.addEventListener('scroll',debounce(function(){
-    animeScroll();
-  }, 50),{passive: true})
+  if(target.length){
+    window.addEventListener('scroll',debounce(function(){
+      animeScroll();
+    }, 50),{passive: true})
+  }
 }
+initAnimeScroll();
